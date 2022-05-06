dddb5cf3d98444a65180d41726777733c8acb5a6
Lia Pratomo <lia.prtm@gmail.com>
2022-05-06 14:43:23 -0700

    Add projects for Data Structure and Performance Cert

---
+++ DS-And-Performance-Cert/MOOCTextEditor/src/spelling/AutoComplete.java
@@ -0,0 +1,11 @@
+/** */
+package spelling;
+
+import java.util.List;
+
+/**
+ * @author Christine
+ */
+public interface AutoComplete {
+    public List<String> predictCompletions(String prefix, int numCompletions);
+}
